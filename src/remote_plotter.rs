7bbf0927fa6b2fc03f3b158160321ce3bd2605ff
Bryan Gale <bryan@bryangale.com>
2024-01-24 16:32:03 +0000

    Update RemotePlotter to use axidraw_over_http

--- src/remote_plotter.rs
+++ src/remote_plotter.rs
@@ -1,46 +1,47 @@
 use crate::{plottable::PlotPoint, plotter::Plotter};
 use reqwest::blocking::Client;
-use std::{collections::HashMap, thread::sleep, time::Duration};
+use std::{cmp::max, str::FromStr, thread::sleep, time::Duration};
 
-static PEN_URL: &str = "http://localhost:4242/v1/pen";
-static MOTORS_URL: &str = "http://localhost:4242/v1/motors";
+static URL: &str = "http://localhost:7878";
 
 pub struct RemotePlotter {
     client: Client,
-    x_steps: i32,
-    y_steps: i32,
 }
 
 impl RemotePlotter {
-    pub fn new(x_steps: i32, y_steps: i32) -> RemotePlotter {
-        RemotePlotter {
+    pub fn new() -> RemotePlotter {
+        let result = RemotePlotter {
             client: Client::new(),
-            x_steps,
-            y_steps,
-        }
+        };
+        result.push("EM,0,0");
+        result
     }
 
     fn pen_up(&self) {
-        let map = HashMap::from([("state", 0)]);
-        self.put(&map);
+        self.push("SP,1");
         sleep(Duration::from_millis(300));
     }
 
     fn pen_down(&self) {
-        let map = HashMap::from([("state", 1)]);
-        self.put(&map);
+        self.push("SP,0");
     }
 
     fn pen_move(&self, x: i32, y: i32) {
-        let map = HashMap::from([
-            ("x", x as f32 * 100.0 / self.x_steps as f32),
-            ("y", y as f32 * 100.0 / self.y_steps as f32),
-        ]);
-        self.put(&map);
+        let a = (x + y).abs();
+        let b = (x - y).abs();
+
+        let max_steps = max(a, b);
+        let duration = (max_steps as f32 * 0.5).round() as i32;
+
+        self.push(format!("XM,{},{},{}", duration, x, y).as_str());
     }
 
-    fn put<T: serde::ser::Serialize>(&self, map: &HashMap<&str, T>) {
-        let result = self.client.put(PEN_URL).json(&map).send();
+    fn push(&self, command: &str) {
+        let result = self
+            .client
+            .post(URL)
+            .body(String::from_str(command).unwrap())
+            .send();
 
         match result {
             Ok(_) => {}
@@ -69,6 +70,6 @@ impl Plotter for RemotePlotter {
 impl Drop for RemotePlotter {
     fn drop(&mut self) {
         self.pen_move(0, 0);
-        let _ = self.client.delete(MOTORS_URL).send();
+        self.push("EM,0,0");
     }
 }
